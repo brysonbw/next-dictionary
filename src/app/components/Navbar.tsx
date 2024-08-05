e75ea38bce36451a09ab48e01b9421cd6a220c47
brysonbw <brysonward77@gmail.com>
2024-08-05 04:45:13 -0400

    mobile title style edits

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* App Name Mobile View */}
-            <p className="group ml-4 relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <p className="group ml-4 relative inline-flex items-center justify-center">
               Dictionary
             </p>
           </div>
